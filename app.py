0ca3f5b4e5d3396e2c9c7e4294f120300d137075
Petra Petrusova <petrusovap03@gmail.com>
2023-11-15 15:44:32 +0000

    default structure for main.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 ''' main application, launching UI, function for processing data '''
+def main():
+    # from sensors.py
+    initialize_sensors()
+    # from actuators.py
+    initialize_actuators()
+    # from gui.py
+    launch_gui()
+    # main loop 
+    work()
+
+if __name__ == "__main__":
+    main()
