276e48e374eece5a79113a24c58c90532f9781de
Petra Petrusova <petrusovap03@gmail.com>
2023-11-19 21:50:18 +0000

    Add base structure to sensors.py

--- sensors.py
+++ sensors.py
@@ -1,1 +1,31 @@
-'''separate class for each sensor - read and process sensor data'''
+'''
+Module containing sensors classes.
+Sensors get data from simulator, process them and send data to the controller.
+'''
+
+import simluator
+
+class TemperatureSensor:
+    # get environment data created by simulator
+    def get_data(self):
+        return simluator.get_data("temperature")
+
+class HumiditySensor:
+    # get environment data created by simulator
+    def get_data(self):
+        return simluator.get_data("humidity")
+
+class LightSensor:
+    # get environment data created by simulator
+    def get_data(self):
+        return simluator.get_data("light")
+
+def initialize_sensors():
+    temperature_sensor = TemperatureSensor
+    humidity_sensor = HumiditySensor
+    light_sensor = LightSensor
+    
+    # put sensors into the output dictionary
+    sensors = {"temperature": temperature_sensor, "humidity": humidity_sensor, "light": light_sensor}
+
+    return sensors
