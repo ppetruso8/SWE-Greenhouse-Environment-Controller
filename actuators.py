5c5c6347a7b3da0b7c04905b324a3246afdd0b31
Petra Petrusova <petrusovap03@gmail.com>
2023-11-24 18:56:04 +0000

    Add actuator classes

--- actuators.py
+++ actuators.py
@@ -19,5 +19,88 @@ NOTE: if that is too difficult, we do not need user input (it could be better bu
 but we can only simulate values, and raise some warnings when the environment condition is not 
 optimal and based on that make changes - it's up to you!
 '''
+import random
 
-from simulator import get_environment, set_environment
+class Heater:
+    def __init__(self, environment):
+        self.env = environment
+        # possible temperature change in one step
+        self.change = 0.3
+
+    def change_temp(self, target_temperature: float):
+        current_temp = self.env.get_environment("temperature")
+
+        # gradually change the temperature
+        while current_temp != target_temperature:
+            if current_temp > target_temperature:
+                new_temp = random.uniform(current_temp, current_temp+self.change)
+
+                if new_temp > target_temperature:
+                    current_temp = target_temperature
+                else:
+                    current_temp = new_temp
+            elif current_temp < target_temperature:
+                new_temp = random.uniform(current_temp-self.change, current_temp)
+
+                if new_temp < target_temperature:
+                    current_temp = target_temperature
+                else:
+                    current_temp = new_temp
+            
+            self.env.set_environment("temperature", current_temp)
+
+class Humidifier:
+    def __init__(self, environment):
+        self.env = environment
+        # possible humidity change in one step
+        self.change = 2
+
+    def change_humidity(self, target_humidity: int):
+        current_humidity = self.env.get_environment("humidity")
+
+        # gradually change the humidity
+        while current_humidity != target_humidity:
+            if current_humidity > target_humidity:
+                new_humidity = random.uniform(current_humidity, current_humidity+self.change)
+
+                if new_humidity > target_humidity:
+                    current_humidity = target_humidity
+                else:
+                    current_humidity = new_humidity
+            elif current_humidity < target_humidity:
+                new_humidity = random.uniform(current_humidity-self.change, current_humidity)
+
+                if new_humidity < target_humidity:
+                    current_humidity = target_humidity
+                else:
+                    current_humidity = new_humidity
+            
+            self.env.set_environment("humidity", current_humidity)
+
+class Lights:
+    def __init__(self, environment):
+        self.env = environment
+        # possible light spectrum change in one step
+        self.change = 10
+
+    def change_light(self, target_light: int):
+        current_light = self.env.get_environment("light")
+
+        # gradually change the light
+        while current_light != target_light:
+            if current_light > target_light:
+                new_light = random.uniform(current_light, current_light+self.change)
+
+                if new_light > target_light:
+                    current_light = target_light
+                else:
+                    current_light = new_light
+            elif current_light < target_light:
+                new_light = random.uniform(current_light-self.change, current_light)
+
+                if new_light < target_light:
+                    current_light = target_light
+                else:
+                    current_light = new_light
+            
+            self.env.set_environment("light", current_light)
