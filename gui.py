52191747ced5fdd61670505fb2d50c51afd7cc3a
Petra Petrusova <petrusovap03@gmail.com>
2023-12-02 23:26:07 +0000

    add docstrings and comments to gui

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 '''
-Display real time data readings from greenhouse environment, Environment instance
-Let user set preferred environmental parameters and pass them to controller
+Display real time data readings from greenhouse environment and show appropriate warnings
+if current conditions are not ideal
 
 source: https://docs.python.org/3/library/tkinter.html 
         https://realpython.com/python-gui-tkinter/
@@ -11,9 +11,14 @@ import tkinter as tk
 from tkinter import ttk
 
 def initialize_gui():
+   '''Create GUI window for the greenhouse environment controller with labels for environment variables
+   '''
+   # create main window
    root = tk.Tk()
+   # set the window's title
    root.title("Greenhouse Environment Controller")
 
+   # create and pack labels for environment variables
    current_temperature_label = ttk.Label(root, text="Temperature: --°C")
    current_temperature_label.pack()
 
@@ -26,6 +31,15 @@ def initialize_gui():
    return root, current_temperature_label, current_humidity_label, current_light_label
 
 def update_gui(current_temperature_label, current_humidity_label, current_light_label, temperature, humidity, light):
+   '''Update GUI labels with the current environment data
+   
+   current_temperature_label -- label for displaying temperature 
+   current_humidity_label -- label for displaying humidity
+   current-light-label -- label for displaying light spectrum 
+   temperature -- current temperature in the greenhouse
+   humidity -- current humidity in the greenhouse
+   light -- current light spectrum value in the greenhouse
+   '''
    current_temperature_label.config(text=f"Temperature: {temperature}°C")
    current_humidity_label.config(text=f"Humidity: {humidity}%")
    current_light_label.config(text=f"Light Spectrum: {light}nm")
