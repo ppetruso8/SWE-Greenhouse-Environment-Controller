ca8c9dace571238c70a9b4b41e78af5f93554818
monarimartina <martinamonari.mm@gmail.com>
2023-12-02 20:53:06 +0000

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,89 +6,91 @@ source: https://docs.python.org/3/library/tkinter.html
         https://realpython.com/python-gui-tkinter/
         https://www.geeksforgeeks.org/python-gui-tkinter/
 '''
-
-'''
-Display real time data readings from greenhouse environment
-Option 1: let user set preferred environmental parameters
-Option 2: Raise warnings when environment condition not optimal and solve
-tkinter?
-'''
 import tkinter as tk
 from tkinter import ttk
-
-# Import your modules here
 from controller import Environment, initialize_sensors, initialize_actuators
 
-# Initialize environment, sensors, and actuators
-environment = Environment(25.0, 60, 550)
-sensors = initialize_sensors(environment)
-actuators = initialize_actuators(environment)
 
-def update_readings():
-    # Update the GUI with the latest sensor readings
-    temperature = sensors['temperature'].get_simulator_data()
-    humidity = sensors['humidity'].get_simulator_data()
-    light = sensors['light'].get_simulator_data()
+class Greenhouse:
 
-    current_temperature_label.config(text=f"Temperature: {temperature}°C")
-    current_humidity_label.config(text=f"Humidity: {humidity}%")
-    current_light_label.config(text=f"Light: {light}")
+   def __init__(self):
+      self.temperature = tk.DoubleVar()
+      self.humidity = tk.DoubleVar()
+      self.light = tk.DoubleVar()
 
-    root.after(1000, update_readings)
 
-def change_environment():
-    # Get the values from input fields and send commands to actuators
-    try:
-        new_temp = float(temp_entry.get())
-        new_humidity = int(humidity_entry.get())
-        new_light = int(light_entry.get())
+   def read_data(self):
+      self.temp_input_label = ttk.Label(root, text="Set Temperature (°C):")
+      self.temp_input_label.pack()
+      self.temp_entry = ttk.Entry(root)
+      self.temp_entry.pack()
 
-        actuators['heater'].change_temp(new_temp)
-        actuators['humidifier'].change_humidity(new_humidity)
-        actuators['lights'].change_light(new_light)
-    except ValueError as e:
-        print(f"Input error: {e}")
+      self.humidity_input_label = ttk.Label(root, text="Set Humidity (%):")
+      self.humidity_input_label.pack()
+      self.humidity_entry = ttk.Entry(root)
+      self.humidity_entry.pack()
 
-# Initialize the Tkinter window
-root = tk.Tk()
-root.title("Greenhouse Environment Controller")
+      self.light_input_label = ttk.Label(root, text="Set Light Spectrum (nm):")
+      self.light_input_label.pack()
+      self.light_entry = ttk.Entry(root)
+      self.light_entry.pack()
 
-# Display labels for current environmental readings
-current_temperature_label = ttk.Label(root, text="Temperature: --°C")
-current_temperature_label.pack()
+      # Button to apply changes
+      apply_button = ttk.Button(root, text="Apply Changes", command=Greenhouse.change_environment)
+      apply_button.pack()
 
-current_humidity_label = ttk.Label(root, text="Humidity: --%")
-current_humidity_label.pack()
 
-current_light_label = ttk.Label(root, text="Light: --")
-current_light_label.pack()
+   def update_readings(self):
+      environment = Environment(25.0, 60, 550)
+      sensors = initialize_sensors(environment)
+      temperature = sensors['temperature'].get_simulator_data()   
+      humidity = sensors['humidity'].get_simulator_data()
+      light = sensors['light'].get_simulator_data()
 
-# Entry fields with descriptive labels for user input
-temp_input_label = ttk.Label(root, text="Set Temperature (°C):")
-temp_input_label.pack()
-temp_entry = ttk.Entry(root)
-temp_entry.pack()
+      self.current_temperature_label.config(text=f"Temperature: {temperature}°C")
+      self.current_humidity_label.config(text=f"Humidity: {humidity}%")
+      self.current_light_label.config(text=f"Light: {light}")
+      root = tk.Tk()
+      root.after(1500, Greenhouse.update_readings)
 
-humidity_input_label = ttk.Label(root, text="Set Humidity (%):")
-humidity_input_label.pack()
-humidity_entry = ttk.Entry(root)
-humidity_entry.pack()
 
-light_input_label = ttk.Label(root, text="Set Light Spectrum (nm):")
-light_input_label.pack()
-light_entry = ttk.Entry(root)
-light_entry.pack()
+   def change_environment(self):
+       # Get the values from input fields and send commands to actuators
+      try:
+         self.new_temp = float(Greenhouse.temp_entry.get())
+         self.new_humidity = int(Greenhouse.humidity_entry.get())
+         self.new_light = int(Greenhouse.light_entry.get())
 
-# Button to apply changes
-apply_button = ttk.Button(root, text="Apply Changes", command=change_environment)
-apply_button.pack()
+         environment = Environment(25.0, 60, 550)
+         actuators = initialize_actuators(environment)
+         actuators['heater'].change_temp(self.new_temp)
+         actuators['humidifier'].change_humidity(self.new_humidity)
+         actuators['lights'].change_light(self.new_light)
+      except ValueError as e:
+           print(f"Input error: {e}")
 
-# Start the update loop
-update_readings()
 
-# Run the application
-root.mainloop()
+   def setup_gui(self, root):
+      # Initialize the Tkinter window
+      root = tk.Tk()
+      root.title("Greenhouse Environment Controller")
 
+      # Display labels for current environmental readings
+      self.current_temperature_label = ttk.Label(root, text="Temperature: --°C")
+      self.current_temperature_label.pack()
+
+      self.current_humidity_label = ttk.Label(root, text="Humidity: --%")
+      self.current_humidity_label.pack()
+
+      self.current_light_label = ttk.Label(root, text="Light Spectrum: --")
+      self.current_light_label.pack()
+
+
+if __name__ == "__main__":
+   root = tk.Tk()
+   Greenhouse.setup_gui
+   Greenhouse.update_readings
+   root.mainloop()
 
 
 # from controller import Environment
@@ -143,67 +145,3 @@ root.mainloop()
 #     root.after(500, Greenhouse().update_data)
 
 #     root.mainloop()
-
-
-
-'''
-Display real time data readings from greenhouse environment, Environment instance
-Let user set preferred environmental parameters and pass them to controller
-
-https://realpython.com/python-gui-tkinter/
-
-
-from tkinter import *
-from tkinter import messagebox
-from tkinter.simpledialog import askinteger
-
-top = Tk()
-top.geometry("600x600")
-
-def check():
-   msg=messagebox.showinfo( "Check on your environment", "//")
-
-B = Button(top, text ="Check on your environment", command = check)
-B.place(x=400,y=500)
-
-
-# if the environment is optimal then:
-var1 = StringVar()
-good_message = "the environment is good! :)"
-messageVar1 = Message(top, text = good_message)
-messageVar1.config(bg='lightgreen')
-messageVar1.pack( )
-
-# otherwise:
-var2 = StringVar()
-bad_message = "the environment is not good! :("
-messageVar2 = Message(top, text = bad_message)
-messageVar2.config(bg='red')
-messageVar2.pack( )
-
-
-
-
-def Lightval():
-   light_val = askinteger("Input", "Input the value of Light in your environment")
-   #print(light_val)
-B = Button(top, text ="Light value", command = Lightval)
-B.place(x=300,y=500)
-
-
-def Humidityval():
-   humidity_val = askinteger("Input", "Input the % of Humidity in your environment")
-   #print(humidity_val)
-B = Button(top, text ="Humidity value", command = Humidityval)
-B.place(x=200,y=500)
-
-
-def Temperatureval():
-   temperature_val = askinteger("Input", "Input the temperature in your environment")
-   #print(temperature_val)
-B = Button(top, text ="Temperature value", command = Temperatureval)
-B.place(x=80,y=500)
-
-
-top.mainloop()
-'''
